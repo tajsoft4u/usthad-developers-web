1ed5268ac6a9b5a998efd085ba080bb6f6676355
Asif <asifbadeghar65@gmail.com>
2023-01-26 16:10:52 +0530

    database file changes

--- application/config/database.php
+++ application/config/database.php
@@ -72,7 +72,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 */
 $active_group = 'default';
 $query_builder = TRUE;
-
+// Code Optimized
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
